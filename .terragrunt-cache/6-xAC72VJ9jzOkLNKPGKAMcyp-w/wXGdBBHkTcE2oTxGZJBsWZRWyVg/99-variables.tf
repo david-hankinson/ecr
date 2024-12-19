f2a48c5dd882d2807cfd3a03090295fc97bb3b7f
david-hankinson <davidmhankinson@outlook.com>
2024-12-18 20:31:46 -0500

    feat/module structure

--- .terragrunt-cache/6-xAC72VJ9jzOkLNKPGKAMcyp-w/wXGdBBHkTcE2oTxGZJBsWZRWyVg/99-variables.tf
+++
@@ -1,9 +0,0 @@
-variable "repository_name" {
-  type        = string
-  description = "Name of the ECR repository"
-}
-
-variable "env" {
-  description = "Which environment the infrastructure will be deployed in"
-  type = string
-}
