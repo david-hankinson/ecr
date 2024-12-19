de3492d6837664f7f064afa568d01ea848a4c978
david-hankinson <davidmhankinson@outlook.com>
2024-12-18 20:38:03 -0500

    feat/basic ecr

--- ecr/99-variables.tf
+++ ecr/99-variables.tf
@@ -3,15 +3,6 @@ variable "repository_name" {
   type        = string # This ensures the variable expects a list of strings
 }
 
-
-variable "security_groups" {
-  type = map(object({
-    description = string
-    tags        = map(string)
-  }))
-  description = "Map of security group configurations"
-}
-
 variable "env" {
   description = "Which environment the infrastructure will be deployed in"
   type = string
