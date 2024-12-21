ad751970023dbe69262d035cbeab5171288f08f6
david-hankinson <davidmhankinson@outlook.com>
2024-12-20 19:01:50 -0500

    feat/cicd for ecr

--- .terragrunt-cache/6-xAC72VJ9jzOkLNKPGKAMcyp-w/szaA2z1_3pN3zbHA2POoin5wn-k/98-outputs.tf
+++
@@ -1,7 +0,0 @@
-output "ecr_repository_name" {
-  value = module.ecr.repository_name
-}
-
-output "ecr_repository_arn" {
-  value = module.ecr.repository_arn
-}
--- .terragrunt-cache/6-xAC72VJ9jzOkLNKPGKAMcyp-w/szaA2z1_3pN3zbHA2POoin5wn-k/ecr/01-data.tf
+++
@@ -1,6 +0,0 @@
-data "aws_caller_identity" "current" {}
-data "aws_ecr_repository" "this" {
-  name = module.ecr.repository_name
-
-  depends_on = [module.ecr]
-}
